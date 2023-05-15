806a54ff3a6eedae801fc82c7f80b489b6a0750f
annmaryvinod <annmarypriyavinod@gmail.com>
2023-05-15 14:56:05 +0530

    intro file is added

---
+++ intro/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+const d = new Date();
+let year = d.getFullYear();
+
+
+const Footer = () => {
+    return ( 
+
+
+        <footer>
+            <p>Copyright All rights reserved {year} </p>
+        </footer>
+     );
+}
+ 
+export default Footer;
